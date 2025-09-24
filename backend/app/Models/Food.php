fa2e4749d3301fa4239af29e1aab438531084622
Kidusdybala <100593162+Kidusdybala@users.noreply.github.com>
2025-09-24 23:28:40 +0300

    Update models to use MongoDB\Laravel\Eloquent\Model

--- backend/app/Models/Food.php
+++ backend/app/Models/Food.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Food extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'restaurant',
+        'price',
+        'image',
+        'description'
+    ];
 }
--- backend/app/Models/Restaurant.php
+++ backend/app/Models/Restaurant.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'cuisine',
+        'rating',
+        'delivery_time',
+        'delivery_fee',
+        'image',
+        'description'
+    ];
 }
